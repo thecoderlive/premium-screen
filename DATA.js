844b9c5f3c2facdda718fd20d6e76c0430c9a614
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-12 13:07:51 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,68 @@
+export const item = {
+    "id": "449f80fe-f4ea-4ed9-9734-a938d3201ff9",
+    "graph_data": [
+        {
+            "id": "7f819c29-5ed3-4cc6-951d-7ad088ac509f",
+            "graph_title": "act",
+            "about_graph": "Say main point join. Follow choice show.",
+            "graph": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+        },
+        {
+            "id": "72ed22d3-c3e6-4baa-a73d-0c396903933d",
+            "graph_title": "which",
+            "about_graph": "Which continue reach away modern voice. Financial support national industry pick. Official remember a look. Still thus word head admit thus.",
+            "graph": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+        },
+        {
+            "id": "25e93329-4d8d-43c9-9a10-8175fc752c36",
+            "graph_title": "baby",
+            "about_graph": "Million address than occur investment something job wife.",
+            "graph": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/268.png"
+        },
+        {
+            "id": "29dbd2a1-76ce-46ac-a5d4-0c4ca8b29f28",
+            "graph_title": "name",
+            "about_graph": "Admit may get remember learn activity wife. Sense wife most game student third manage after.",
+            "graph": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+        },
+        {
+            "id": "91e4635a-345f-4592-b6ba-63a85d4b9603",
+            "graph_title": "service",
+            "about_graph": "Add pass pick write father speech physical. Glass sure somebody watch task drive machine. Around why her official develop.",
+            "graph": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+        }
+    ],
+    "premium_detail": [
+        {
+            "id": "ef9dde2f-122f-46cc-9015-c005946198c3",
+            "duration": "about",
+            "charges": "imagine",
+            "save_percentage": "community"
+        },
+        {
+            "id": "76c6d16c-cc4b-470a-a349-daad621682c9",
+            "duration": "range",
+            "charges": "list",
+            "save_percentage": "debate"
+        },
+        {
+            "id": "bbad0e2b-0df7-4d79-949f-10c933daa6cf",
+            "duration": "nation",
+            "charges": "see",
+            "save_percentage": "throw"
+        },
+        {
+            "id": "e8799170-cef8-48ac-a2fd-e452b817adb6",
+            "duration": "we",
+            "charges": "travel",
+            "save_percentage": "physical"
+        },
+        {
+            "id": "3e00b9b1-c234-46d1-9ffc-3917307c954c",
+            "duration": "east",
+            "charges": "four",
+            "save_percentage": "cost"
+        }
+    ],
+    "about_premium": "Option most particular something purpose loss country. More drug impact region instead. Or their court despite beyond all."
+}
